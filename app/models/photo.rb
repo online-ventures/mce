986c56ac4861b2479c36a819ce43a25990b3ba47
Jacob Shreve <jacob@shreve.ly>
2013-06-27 22:34:21 -0400

    Bad update fixed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,7 +5,7 @@ class Photo < ActiveRecord::Base
   attr_accessible :image, :vehicle_id, :vehicles_photo_id, :deleted_at, :featured?
   belongs_to :vehicle, touch: true
 
-  has_attached_file :image, styles: { thumb: '150x150#' }, s3_permissions: :read
+  has_attached_file :image, styles: { thumb: '150x150#' }
   validates_attachment :image, presence: {message: 'must be chosen before saving'}
 	validates_attachment :image, content_type: {content_type: ['image/jpeg','image/jpg','image/png','image/gif','application/zip']}
   before_post_process :image?
