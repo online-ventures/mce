0743597719067b6d4d98c2abfc18ffced3affdc9
Jacob Evan Shreve <jacob@shreve.ly>
2013-08-20 09:58:14 -0400

    Test-related changes

--- test/unit/vehicle_test.rb
+++ test/unit/vehicle_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class VehicleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "vehicle should only require year, make, and model" do
+		vehicle = Vehicle.new
+		assert !vehicle.save, "Vehicle able to save without year, make, and model"
+
+		vehicle.year = strftime('%Y').to_i
+		vehicle.model = model(:one)
+		vehilce.make = make(:one)
+		assert vehicle.save, "Vehicle unable to save with just year, make, and model"
+	end
 end
