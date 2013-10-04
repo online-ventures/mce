2b1a852af826f6a0e032feb8e8c2051c8bfd37c3
Jacob Evan Shreve <jacob@shreve.ly>
2013-10-04 17:03:34 -0400

    another paperclip name update

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
-location = '/:vehicle_id/:vehicle.:extension'
+location = '/:vehicle_id/:name.:extension'
 
 url = PROD ? ':s3_domain_url' : '/uploads' + location
 Paperclip::Attachment.default_options[:url] = url
@@ -10,5 +10,4 @@ Paperclip::Attachment.default_options[:default_style] = :original
 
 #http://viget.com/extend/paperclip-custom-interpolation
 Paperclip.interpolates(:vehicle_id)    {|a,s| a.instance.vehicle_id }
-Paperclip.interpolates(:vehicle)       {|a,s| a.instance.vehicle_string || "#{a.instance.vehicle.to_s('_')}_#{a.instance.vehicles_photo_id}" }
-Paperclip.interpolates(:count)         {|a,s| a.instance.vehicles_photo_id }
+Paperclip.interpolates(:name)       {|a,s| a.instance.vehicle_string || a.instance.vehicles_photo_id }
