de4712723fc4a780e2c6700be53665c91e87abcf
Jacob Evan Shreve <jacob@shreve.ly>
2014-04-11 13:32:07 -0400

    Update logo and menu

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -9,30 +9,24 @@
 #= require_directory ./application/
 #= require_tree .
 
-$(window).resize ->
-	$this = $(this)
-	topbars = $('body > .topbar')
-	$topbar = $(topbars[0])
-	$subnav = $(topbars[1])
+window.smallWidth = $('meta.foundation-mq-small').css('width')
+window.mediumWidth = $('meta.foundation-mq-medium').css('width')
+window.largeWidth = $('meta.foundation-mq-large').css('width')
 
-	min_width = 0
-	$topbar.find('h1,img').map ->
-		min_width += $(this).outerWidth()
-	if $this.outerWidth() < min_width
-		$topbar.css 'min-height', ($topbar.find('h1').outerHeight() * 2) + 'px'
-		$topbar.find('h1').removeClass('right').addClass('center')
-	else
-		$topbar.css 'min-height', $topbar.find('h1').outerHeight() + 'px'
-		$topbar.find('h1').removeClass('center')
-		$topbar.find('h1').last().addClass('right')
-	if $subnav.outerHeight() >= $subnav.find('li').outerHeight() * 2
-		$subnav.find('li').map ->
-			if $(this).html().search(/about|buyer/) > 1
-				$(this).hide()
+$menu = $('#mobile-menu')
+
+$(document).ready ->
+  $menu.val(window.location.pathname)
+
+$menu.on 'blur change', ->
+  $this = $(this)
+  unless $this.val() is window.location.pathname
+    window.location.pathname = $this.val()
+
 $body = $('body')
 content_height = 0
 $body.children().not('script, .typekit-badge, #container').each ->
-	content_height += $(this).outerHeight()
+  content_height += $(this).outerHeight()
 $('body > #container').css('min-height', $(window).height() - content_height)
 
 $(document).foundation()
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,82 +1,92 @@
 module ApplicationHelper
 
-	# Extend time classes
-	time_class = lambda do |a|
-		def short
-			self.strftime '%b %e, %Y'
-		end
+  # Extend time classes
+  time_class = lambda do |a|
+    def short
+      self.strftime '%b %e, %Y'
+    end
 
-		def long
-			self.strftime '%B %e, %Y'
-		end
+    def long
+      self.strftime '%B %e, %Y'
+    end
 
-		def weekday
-			self.strftime '%A'
-		end
-	end
-	Time.class_eval &time_class
-	DateTime.class_eval &time_class
+    def weekday
+      self.strftime '%A'
+    end
+  end
+  Time.class_eval &time_class
+  DateTime.class_eval &time_class
 
-	# Return the interpolated template text if the variable has a value.
-	# http://stackoverflow.com/a/1697919/1893290
-	#
-	# show_if " updated $1 ago.", time_ago_in_words(@vehicle.updated_at), @vehicle.updated_at != @vehicle.created_at
-	#
-	# > " Updated 12 days ago."
-	#
-	def show_if(template, variable, condition = nil, &block)
-		# if no condition, use variable, optionally, use condition instead
-		if (condition.nil? && variable) || (!condition.nil? && !!condition)
-			# if we pass a block, give that instead
-			if block_given?
-				yield block
-			elsif template.match /\$1/
-				raw template.gsub('$1', variable)
-			end
-		else
-			''
-		end
-	end
+  def menu_elements(&block)
+    {
+      'Vehicles' => inventory_vehicles_path,
+      'About' => '/about',
+      'Buyer Info' => '/buyer-info',
+      'FAQ' => '/faq',
+      'Contact' => '/contact'
+    }.each(&block)
+  end
 
-	def index_title_helper
-		klass = eval(params[:controller].classify)
-		raw "<h1>Listing #{'Deleted' if params[:deleted] and params[:deleted] == 'true'} #{klass.to_s.pluralize.capitalize}</h1>"
-	end
+  # Return the interpolated template text if the variable has a value.
+  # http://stackoverflow.com/a/1697919/1893290
+  #
+  # show_if " updated $1 ago.", time_ago_in_words(@vehicle.updated_at), @vehicle.updated_at != @vehicle.created_at
+  #
+  # > " Updated 12 days ago."
+  #
+  def show_if(template, variable, condition = nil, &block)
+    # if no condition, use variable, optionally, use condition instead
+    if (condition.nil? && variable) || (!condition.nil? && !!condition)
+      # if we pass a block, give that instead
+      if block_given?
+        yield block
+      elsif template.match /\$1/
+        raw template.gsub('$1', variable)
+      end
+    else
+      ''
+    end
+  end
 
-	def new_record_button(klass)
-		link_to "New #{klass.to_s.humanize}", eval("new_#{klass.to_s.tableize.singularize}_path"), class: 'button'
-	end
+  def index_title_helper
+    klass = eval(params[:controller].classify)
+    raw "<h1>Listing #{'Deleted' if params[:deleted] and params[:deleted] == 'true'} #{klass.to_s.pluralize.capitalize}</h1>"
+  end
 
-	def toggle_deleted_view_button(klass)
-		if klass.unscoped.where('deleted_at IS NOT NULL').count > 0
-			if params[:deleted] and params[:deleted] == 'true'
-				link_to "Show Active #{klass.to_s.pluralize}", eval("#{klass.to_s.tableize}_path"), class: 'button right'
-			else
-				link_to "Show Deactivated #{klass.to_s.pluralize}", '?deleted=true', class: 'button right'
-			end
-		end
-	end
+  def new_record_button(klass)
+    link_to "New #{klass.to_s.humanize}", eval("new_#{klass.to_s.tableize.singularize}_path"), class: 'button'
+  end
 
-	def new_and_toggle_buttons
-		klass = eval(params[:controller].classify)
-		"<div class='row'>
+  def toggle_deleted_view_button(klass)
+    if klass.unscoped.where('deleted_at IS NOT NULL').count > 0
+      if params[:deleted] and params[:deleted] == 'true'
+        link_to "Show Active #{klass.to_s.pluralize}", eval("#{klass.to_s.tableize}_path"), class: 'button right'
+      else
+        link_to "Show Deactivated #{klass.to_s.pluralize}", '?deleted=true', class: 'button right'
+      end
+    end
+  end
+
+  def new_and_toggle_buttons
+    klass = eval(params[:controller].classify)
+    "<div class='row'>
       <div class='large-3 columns'>
-        #{new_record_button(klass)}
+    #{new_record_button(klass)}
       </div>
       <div class='large-4 columns'>
-        #{toggle_deleted_view_button(klass)}
+    #{toggle_deleted_view_button(klass)}
       </div>
     </div>".html_safe
-	end
+  end
 
-	def manage_buttons(record)
-		output = ''
-		if params[:deleted] and params[:deleted] == 'true'
-			output << link_to('Reactivate', eval("restore_#{record.class.to_s.downcase}_path(record)"), method: :put, class: 'small success button')
-		else
-			output << link_to('Edit', eval("edit_#{record.class.to_s.tableize.singularize}_path(record)"), class: 'small secondary button')
-			output << link_to('Deactivate', record, method: :delete, data: {confirm: 'Are you sure?'}, class: 'small alert button')
-		end
-		raw output
-	end
+  def manage_buttons(record)
+    output = ''
+    if params[:deleted] and params[:deleted] == 'true'
+      output << link_to('Reactivate', eval("restore_#{record.class.to_s.downcase}_path(record)"), method: :put, class: 'small success button')
+    else
+      output << link_to('Edit', eval("edit_#{record.class.to_s.tableize.singularize}_path(record)"), class: 'small secondary button')
+      output << link_to('Deactivate', record, method: :delete, data: {confirm: 'Are you sure?'}, class: 'small alert button')
+    end
+    raw output
+  end
 end
